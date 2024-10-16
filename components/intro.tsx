446a6c482ca6b6e652330a4d44fbcc5c9f7ed128
Mary Hayne <mahayne@gmail.com>
2024-10-16 15:44:57 -0700

    fix lost work (#17)

--- components/intro.tsx
+++ components/intro.tsx
@@ -8,14 +8,16 @@ export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-8 gap-y-4 pb-24 pr-16 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
-        <h2 className='title no-underline'>Hey, Mary here. Welcome in!</h2>
+        <h2 className='title no-underline'>
+          Hello! Mary here. Nice to meet you.
+        </h2>
         <p className='mt-3 font-light text-muted-foreground'>
           I&#39;m a frontend engineer and ux designer based in San Francisco,
           CA. I&#39;m passionate about learning new technologies that help me
           make the user experience beautiful, accessible, and performant.
         </p>
       </div>
-      <div className='relative w-48 pt-8'>
+      <div className='relative w-48 self-end pt-8'>
         <Image
           className='flex-1 rounded-lg grayscale hover:filter-none'
           src={authorImage}
