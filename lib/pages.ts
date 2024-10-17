31e0c3628e303bcc7f008d17997c133e82d93080
Mary Hayne <mahayne@gmail.com>
2024-10-16 22:06:51 -0700

    update page with recent img data changes (#19)

--- lib/pages.ts
+++ lib/pages.ts
@@ -1,27 +1,18 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-const sizeOf = require('image-size')
+import { getImageData } from './projects'
 
 const rootDirectory = path.join(process.cwd(), 'content', 'pages')
-const imageDirectory = path.join(process.cwd(), 'public')
 
 export type PageData = {
   metadata: PageMetadata
   content: string
 }
-export type PageHeaderImage = {
-  image: string
-}
+
 export type PageMetadata = {
   title?: string
   summary?: string
-  thumbData?: {
-    src: string
-    width: number
-    height: number
-    type?: string
-  } | null
   imageData?: {
     src: string
     width: number
@@ -38,18 +29,11 @@ export async function getPageBySlug(slug: string): Promise<PageData | null> {
     const filePath = path.join(rootDirectory, `${slug}.mdx`)
     const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' })
     const { data, content } = matter(fileContent)
-    const imageData = getImageData(data.image)
-    const thumbData = data.thumb !== undefined ? getImageData(data.thumb) : null
-    return { metadata: { ...data, slug, thumbData, imageData }, content }
+    const imageData = data.image && getImageData(data.image)
+    return { metadata: { ...data, slug, imageData }, content }
   } catch (error) {
     return null
   }
-}
-
-export function getImageData(filepath: string): PageMetadata['imageData'] {
-  const imagePath = path.join(imageDirectory, filepath)
-  const dimensions = sizeOf(imagePath)
-  return dimensions
 }
 
 export function getPageMetadata(filepath: string): PageMetadata {
@@ -57,7 +41,7 @@ export function getPageMetadata(filepath: string): PageMetadata {
   const filePath = path.join(rootDirectory, filepath)
   const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' })
   const { data } = matter(fileContent)
-  const imageData = getImageData(data.image)
-  const thumbData = data.thumb !== undefined ? getImageData(data.thumb) : null
-  return { ...data, slug, thumbData, imageData }
+  const imageData = data.image && getImageData(data.image)
+
+  return { ...data, slug, imageData }
 }
