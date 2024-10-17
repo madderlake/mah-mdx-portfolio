841ad9648099f7f429e394ae0035d9eef71e7e38
Mary Hayne <mahayne@gmail.com>
2024-10-17 14:55:56 -0700

    responsive styling (#20)

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,18 +6,18 @@ export default function Intro() {
   const imageData = getImageData(authorImage)
 
   return (
-    <section className='flex flex-col-reverse items-start gap-x-8 gap-y-4 pb-24 pr-16 md:flex-row md:items-center'>
-      <div className='mt-2 flex-1 md:mt-0'>
+    <section className='flex flex-col-reverse items-start justify-center gap-x-8 gap-y-4 pb-24 sm:flex-row sm:items-center sm:pr-16'>
+      <div className='mt-2 flex-1 text-center sm:mt-0 sm:text-left'>
         <h2 className='title no-underline'>
           Hello! Mary here. Nice to meet you.
         </h2>
         <p className='mt-3 font-light text-muted-foreground'>
           I&#39;m a frontend engineer and ux designer based in San Francisco,
-          CA. I&#39;m passionate about learning new technologies that help me
-          make the user experience beautiful, accessible, and performant.
+          CA. I&#39;m passionate about learning new technologies that make the
+          user experience beautiful, accessible, and performant.
         </p>
       </div>
-      <div className='relative w-48 self-end pt-8'>
+      <div className='relative w-48 self-center pt-8 sm:self-end'>
         <Image
           className='flex-1 rounded-lg grayscale hover:filter-none'
           src={authorImage}
