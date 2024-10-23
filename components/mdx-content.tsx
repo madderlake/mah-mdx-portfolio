184f189b2a391c04c3c81d79a6d6288d43326842
madderlake <mary@maryahayne.com>
2024-10-22 23:33:55 -0700

    repl image with next/image

--- components/mdx-content.tsx
+++ components/mdx-content.tsx
@@ -1,16 +1,38 @@
 import { JSX } from 'react'
 import { highlight } from 'sugar-high'
 import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc'
-
+import Image, { ImageProps } from 'next/image'
+import sizeOf from 'image-size'
 import Counter from '@/components/counter'
+import path from 'path'
 
+// Custom component to handle code highlighting
 function Code({ children, ...props }: any) {
-  let codeHTML = highlight(children)
+  const codeHTML = highlight(children)
   return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
 }
 
+// Custom Next/Image component for markdown
+const MarkdownImage = ({ src, alt }: ImageProps) => {
+  const imageDirectory = path.join(process.cwd(), 'public')
+  const dir = path.join(imageDirectory, src as string)
+  const imgSize = sizeOf(dir as string)
+  return (
+    <Image
+      src={src}
+      alt={alt || 'Image'}
+      width={imgSize?.width}
+      height={imgSize?.height}
+      fill={imgSize === undefined}
+      style={{ maxWidth: '100%', height: 'auto' }}
+    />
+  )
+}
+
+// Define custom components to override default markdown rendering
 const components = {
   code: Code,
+  img: (props: any) => <MarkdownImage {...props} />,
   Counter
 }
 
